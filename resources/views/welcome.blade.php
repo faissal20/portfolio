472075155b04498b63486aae491ed7443a321024
faissal20 <fayssalz15z@gmail.com>
2024-02-07 07:12:50 +0100

    update tas... page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,6 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        
-        <h1>
-            Good Morning My Everything !
-        </h1>
-
-        <p>
-            You know since you are busy this days. 
-        </p>
-
-        <form action="" method="post" id="form">
-            @csrf
-            <div class="input hidden" id="messageInput">
-                <label for="message">leave a message for me </label>
-                <input type="text" placeholder="Message" id="message" name="message">
-            </div>
-            <button type="submit" class="primary-btn">Forgive me !</button>
-        </form>
+        Soon !
     </div>
 @endsection
